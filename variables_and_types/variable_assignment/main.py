9b3c667f948ae9a2e49a9352a38056c86cffae1a
flashhowling <>
2026-01-31 05:18:52 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,41 @@
+
+
+
+# Testing
+#print("Item : ", item_name)
+#print("Price per unit: $", item_price)
+#print("quantity in stock: ", item_stock_quantity)
+#print("In stock: ", in_stock) 
+
+
+#("Item_name = banana")
+#("Item_price = 0.50")
+#("Item_stock_quantity = 100")
+#("in_stock = True")
+
+#print("Item:", Item_name)
+#print("price: $", Item_price)
+#print("Quantity:", Item_stock_quantity)
+#print("in stock:", In_stock)
+
+Item_name = "banana"
+Item_price = 0.50
+Item_stock_quantity = 100
+in_stock = True
+
+item_name = "banana"
+print("Item:", Item_name)
+
+item_price = 0.50
+print("price: $", Item_price)
+
+item_stock_quantity = 100
+print("Quantity:", Item_stock_quantity)
+
+in_stock = True
+print("in stock:", in_stock)
+
+item_name = "Eggs"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
