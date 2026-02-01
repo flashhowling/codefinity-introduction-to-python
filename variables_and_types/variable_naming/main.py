abce5d845607e138e824fea05143cc18a6e5cbd4
flashhowling <>
2026-02-01 20:28:28 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,24 @@
+
+
+
+# Testing
+#print("Item:", ___)
+#print("Price per loaf: $", ___)
+#print("Quantity in stock:", ___)
+
+
+item_name = "Bread"
+item_price = 4.52
+item_in_stock = 230
+
+print("item:", )
+print("price per loaf: $", )
+print("quantity in stock:",)
+
+
+
+#Item_name = "Apple"
+#_item_price = 0.79
+#item1_quantity = 10
+#storename = "green valley groceries"
+#print("item_name, _item_price, item1_quantity, storename")
